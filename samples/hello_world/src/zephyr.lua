f7333b7515387f328a09871b3a00c698ebd47cf5
Victor Miranda <varm@ic.ufal.br>
2025-05-24 02:02:42 -0300

    feat: Add logging to the hello_world sample

--- samples/hello_world/src/zephyr.lua
+++ samples/hello_world/src/zephyr.lua
@@ -2,5 +2,15 @@
 zephyr = {}
 
 function zephyr.msleep(duration_ms) end
+
 function zephyr.println(message) end
+
 function zephyr.print(message) end
+
+function zephyr.log_inf(message) end
+
+function zephyr.log_wrn(message) end
+
+function zephyr.log_dbg(message) end
+
+function zephyr.log_err(message) end
