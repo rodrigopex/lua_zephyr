b956de7205f6abed9c5a7bd63047e87b3b047e9c
Victor Miranda <varm@ic.ufal.br>
2026-02-02 22:18:09 -0300

    docs: Document the codec usage in the producer_consumer sample

--- samples/producer_consumer/src/channels.c
+++ samples/producer_consumer/src/channels.c
@@ -4,13 +4,17 @@
 #include <zephyr/sys/util.h>
 #include <lua_zephyr/codec.h>
 
+/* An example of a table to struct mapping descriptor. This maps a Lua table
+ * with fields 'x', 'y', 'z' and 'source' to the struct msg_acc_data. The process of encoding
+ * and decoding is then automated using this descriptor. */
 static const struct lua_zephyr_table_descr acc_data[] = {
 	LUA_TABLE_FIELD_DESCRIPTOR_PRIM(struct msg_acc_data, x, LUA_CODEC_VALUE_TYPE_INTEGER),
 	LUA_TABLE_FIELD_DESCRIPTOR_PRIM(struct msg_acc_data, y, LUA_CODEC_VALUE_TYPE_INTEGER),
 	LUA_TABLE_FIELD_DESCRIPTOR_PRIM(struct msg_acc_data, z, LUA_CODEC_VALUE_TYPE_INTEGER),
 	LUA_TABLE_FIELD_DESCRIPTOR_PRIM(struct msg_acc_data, source, LUA_CODEC_VALUE_TYPE_STRING),
 };
-LUA_ZEPHYR_WRAPPER_DESC(acc_data);
+LUA_ZEPHYR_WRAPPER_DESC(acc_data); /* This creates a user_data_wrapper that's embedded
+				    * in the channel definition below */
 
 static const struct lua_zephyr_table_descr acc_data_array[] = {
 	LUA_TABLE_FIELD_DESCRIPTOR_ARRAY(struct msg_acc_data_array, data,
@@ -33,6 +37,8 @@ static const struct lua_zephyr_table_descr version[] = {
 };
 LUA_ZEPHYR_WRAPPER_DESC(version);
 
+/* The actual channel definitions, embedding the user_data_wrapper
+ * created above. */
 ZBUS_CHAN_DEFINE(chan_acc_data, struct msg_acc_data, NULL, (void *)&ud_acc_data,
 		 ZBUS_OBSERVERS_EMPTY, ZBUS_MSG_INIT(.x = 0, .y = 0, .z = 0, .source = ""));
 
