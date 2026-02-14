dfe29db2b63e8cfc684f9c6c251ea4d97a64a37e
Rodrigo Peixoto <rodrigopex@gmail.com>
2026-02-14 19:17:59 -0300

    chore: add comments documenting extra optimization defaults in CMakeLists

--- samples/producer_consumer/src/zlua_apis.lua
+++ samples/producer_consumer/src/zlua_apis.lua
@@ -1,9 +1,10 @@
-module("zephyr")
 ---@meta
 zephyr = {}
 
 function zephyr.msleep(duration_ms) end
+
 function zephyr.println(message) end
+
 function zephyr.printk(message) end
 
 zbus = {}
@@ -13,8 +14,10 @@ zbus = {}
 ---@param data integer # Random interger to publish.
 ---@param timeout integer # Publication timeout.
 function zbus.chan_pub(chan, data, timeout) end
+
 function zbus.sub_wait_msg(obs, chan, timeout) end
 
 struct = {}
 function struct.pack(fmt, data, ...) end
+
 function struct.unpack(fmt, s) end
